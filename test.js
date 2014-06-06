d1f20aaf66ed0ecc64aa0a51497ab4f332230b5b
Travis Beauvais <tbeauvais@gmail.com>
2014-06-06 09:53:39 -0700

    Outbound Javascript client

---
+++ test.js
@@ -0,0 +1,35 @@
+var outbound = require("./outbound");
+
+var ob = new outbound("myTestApiKey");
+
+/*
+The only things we want to test are to ensure we are forcing user IDs and event
+as the proper types. User IDs must be string or number and events must be strings.
+
+We don't need a success callback because the HTTP calls will always fail triggering
+an error.
+*/
+
+ob.identify([1,2]).error(
+    function(err) {
+        if (err.receivedCall) {
+            console.log("[FAIL - identify] Should have recieved bad user ID error.");
+        }
+    }
+);
+
+ob.track([1,2], "1").error(
+    function(err) {
+        if (err.receivedCall) {
+            console.log("[FAIL - track] Should have recieved bad user ID error.");
+        }
+    }
+);
+
+ob.track(1, 1).error(
+    function(err) {
+        if (err.receivedCall) {
+            console.log("[FAIL - track] Should have recieved bad event error.");
+        }
+    }
+);
